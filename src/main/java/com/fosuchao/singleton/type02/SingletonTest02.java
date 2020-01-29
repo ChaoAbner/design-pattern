7e218b90882c379d39acd79b0d7bb7d99adb3662
ChaoAbner <2473109110@qq.com>
2020-01-29 11:00:05 +0800

    type of hungry man using static code block

---
+++ src/main/java/com/fosuchao/singleton/type02/SingletonTest02.java
@@ -0,0 +1,15 @@
+package com.fosuchao.singleton.type02;
+
+/**
+ * @Description:
+ * @Auther: Joker Ye
+ * @Date: 2020/1/29 10:41
+ */
+public class SingletonTest02 {
+
+}
+
+
+class Singleton{
+
+}
