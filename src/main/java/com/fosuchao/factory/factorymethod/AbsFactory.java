ad0de90dd4852b022d9bea2bed36c47c1599da89
ChaoAbner <2473109110@qq.com>
2020-01-31 10:29:31 +0800

    修改示例

---
+++ src/main/java/com/fosuchao/factory/factorymethod/AbsFactory.java
@@ -0,0 +1,10 @@
+package com.fosuchao.factory.factorymethod;
+
+/**
+ * @Description:
+ * @Auther: Joker Ye
+ * @Date: 2020/1/31 10:20
+ */
+public interface AbsFactory {
+    Sender createMail();
+}
