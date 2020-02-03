6a9f16b2f3d86e400c95f12effd376ab80c2d9b0
ChaoAbner <2473109110@qq.com>
2020-02-03 17:23:24 +0800

    适配器模式

--- src/main/java/com/fosuchao/adapter/interfaceAdapter/AudioPlayer.java
+++ src/main/java/com/fosuchao/adapter/interfaceAdapter/AudioPlayer.java
@@ -9,21 +9,21 @@ public class AudioPlayer implements Player {
 
     @Override
     public void play(String mediaType, String filename) {
-       if("mp3".equalsIgnoreCase(mediaType)) {
-           this.Mp3Player(filename);
-       } else if ("vlc".equalsIgnoreCase(mediaType)
-       || "mp4".equalsIgnoreCase(mediaType)) {
-           new MediaAdapter(mediaType).play(mediaType, filename);
-       } else {
-           this.printTypeError();
-       }
+        if ("mp3".equalsIgnoreCase(mediaType)) {
+            this.Mp3Player(filename);
+        } else if ("vlc".equalsIgnoreCase(mediaType)
+                || "mp4".equalsIgnoreCase(mediaType)) {
+            new MediaAdapter(mediaType).play(mediaType, filename);
+        } else {
+            this.printTypeError();
+        }
     }
 
-    public void Mp3Player(String filename){
+    public void Mp3Player(String filename) {
         System.out.println("播放MP3音频，文件为：" + filename);
     }
 
-    public void printTypeError(){
+    public void printTypeError() {
         System.out.println("不支持的音频类型");
     }
 
